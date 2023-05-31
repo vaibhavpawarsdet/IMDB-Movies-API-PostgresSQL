a650ae305c9cf0dc7b1c7f1a4ffae7a507950cdd
vaibhavpawarsdet <vaibhavpawarsdet@gmail.com>
2023-05-31 19:07:41 +0530

    json updated

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const db = require('./app/models');
+
+dotenv.config();
+
+const app = express();
+
+const corsOptions = { origin: "http://localhost:5000" };
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+db.sequelize.sync().then(() => {
+    console.log("db synced and connected successfully");
+}).catch((err) => {
+    console.log("failed to sync db: " + err.message);
+}); 
+
+require("./app/routes/movie.routes.js")(app);
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+    console.log(`server running port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+    res.json({ message: "starting new application"});
+});
