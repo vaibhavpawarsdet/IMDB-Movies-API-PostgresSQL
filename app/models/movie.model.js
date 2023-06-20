745ebda8f5a2f0b2e5d7975080b141736e83ed11
vaibhavpawarsdet <vaibhavpawarsdet@gmail.com>
2023-06-20 10:15:34 +0530

    movies model updated

--- app/models/movie.model.js
+++ app/models/movie.model.js
@@ -1,17 +1,20 @@
 module.exports = (sequelize, Sequelize) => {
     const Movie = sequelize.define("movie", {
-        title: { 
+        title: {
             type: Sequelize.STRING
         },
         description: {
             type: Sequelize.STRING
         },
-        rating: {
+        release_date: {
+            type: Sequelize.STRING
+        },
+        language: {
             type: Sequelize.STRING
         },
         published: {
             type: Sequelize.BOOLEAN
         }
-    });
+    }); 
     return Movie;
 };
